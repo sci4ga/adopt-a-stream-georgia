260414681e81fe6a98db8e88740b728be2450f12
Louis Kiphen <greypitch@live.com>
2021-10-25 07:44:25 -0400

    first attempt at extract
    missing data for some fields

--- etl.py
+++ etl.py
@@ -4,13 +4,25 @@ import sys
 import pandas as pd
 from extract import extract
 from transform import transform
-   
-out_file = "adopt-a-stream-georgia-data.csv"
 
+try:
+    if sys.argv[1] is not None:
+        test = True
+except:
+    test = False
+
+out_file = "adopt-a-stream-georgia-data.csv"
+starting_dir = os.getcwd()
+responses_dir = starting_dir + "\\responses\\responses"
 # get the list of html files
-os.chdir("./responses")
-files = [file_name for file_name in os.listdir() if file_name.contains(".html")]
-os.chdir("..")
+
+os.chdir(responses_dir)
+
+files = [file_name for file_name in os.listdir() if ".html" in file_name]
+
+if test:
+    files = files[0:99]
+os.chdir(starting_dir)
 
 # load a data frame with all the field names, xpaths, and elements from the csv.
 html_elements = pd.read_csv("AAS-data-fields-from-html.csv")
@@ -26,10 +38,11 @@ html_elements["XPath"] = html_elements["XPath"].apply(get_xpath)
 data = []
 
 for file in files:
+    print("Extracting from file: " + file)
     row, _ = transform(extract(file, html_elements))
     data.append(row)
 
 # get the header and load the data into a dataframe and out to csv
-_, list_header = transform(extract(files[0]))
+_, list_header = transform(extract(files[0], html_elements))
 dataFrame = pd.DataFrame(data = data, columns = list_header)
 dataFrame.to_csv(out_file)
--- extract.py
+++ extract.py
@@ -5,31 +5,42 @@ from collections import OrderedDict
 import pandas as pd
 
 
-def get_field(soup_in, field):
-    xpath = field["XPath"][0]
-    if xpath[1] != '':
-        element = soup_in.find(id=xpath[0]).get("value")
+def get_field(soup_in, field_in):
+    # print("FIELD_IN =  " + str(type(field_in)))
+    # print(str(field_in))
+    xpath = field_in["XPath"].values[0]
+    if xpath[1] == '':
+        element = soup_in.find(id=xpath[0])
+        if (element.get("checked") is not None) or (element.get("type") in ["radio", "checkbox"]):
+            value = element.get("checked")
+            if value == "checked":
+                value = True
+            else:
+                value = False
+        elif element.get("value") is not None:
+            value = element.get("value")
+        else:
+            value = element.text
+        return value
     else:
         # TODO: handle case where xpath is two-part
-        element = soup_in.find(id=xpath[0]).get("value")
+        return ''
     
     # TODO: handle determining what part of the element contains the data
-    if True:
-        value = element.get("value")
-    else:
-        value = element.text
 
-    return value
 
 def extract(html_file_in, html_elements):
+    starting_dir = os.getcwd()
+    responses_dir = starting_dir + "\\responses\\responses"
+    os.chdir(responses_dir)
     with open(html_file_in) as f:
         file_content = f.read()
         soup = BeautifulSoup(file_content, 'html.parser')
-    
+    os.chdir(starting_dir)
     # build array with extracted data
     data = OrderedDict()
     for n in range(len(html_elements["XPath"].values)):    
         field = html_elements.iloc[[n]]
-        data[html_elements.iloc[[n]]["Field Name"][0]] = get_field(soup, field)
-    
+        # print("FIELD = " + str(field))
+        data[str(field["Field Name"].values[0])] = get_field(soup, field)    
     return data
